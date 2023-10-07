8c6dd3a53dbf46f176849b2f184c4f58bbb912cc
allansalmeida <74943541+allansalmeida@users.noreply.github.com>
2023-10-07 00:44:34 -0300

    Add files via upload
    adicionando app 3

---
+++ app_3/styles.js
@@ -0,0 +1,36 @@
+import {StyleSheet} from 'react-native'
+
+
+
+
+const styles = StyleSheet.create({
+  titulo:{
+    fontSize: 20,
+    color:'red',
+    marginTop: 15,
+    textAlign: 'center'
+  },
+  resultado: {
+    fontSize: 50,
+    textAlign: 'center',
+    color: 'green'
+  },
+  input:{
+    height: 45,
+    borderWidth: 1,
+    borderColor: '#222',
+    margin: 10,
+    fontSize: 20,
+    padding: 10,
+  },
+  fundo:{
+    backgroundColor: 'gray',
+    textAlign: 'center'
+  },
+
+})
+
+
+
+
+export {styles}
