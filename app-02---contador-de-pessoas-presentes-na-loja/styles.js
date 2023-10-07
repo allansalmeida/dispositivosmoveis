b9cc95193b2870264b623ef7d40701a4482f2b07
allansalmeida <74943541+allansalmeida@users.noreply.github.com>
2023-10-07 00:41:27 -0300

    Add files via upload
    Adicionando App 2

---
+++ app-02---contador-de-pessoas-presentes-na-loja/styles.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native'
+
+
+const styles = StyleSheet.create({
+  titulo:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color:'black',
+    marginTop: 15,
+    textAlign: 'center'
+  },
+  contador: {
+    fontSize: 80,
+    textAlign: 'center',
+    color: 'black'
+  }
+})
+
+
+export {styles}
